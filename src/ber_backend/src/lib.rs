c2ce43e3169cec6d938983d8990b2e9f47de24e0
Ognjen Maric <ognjen.maric@dfinity.org>
2024-09-11 18:08:47 +0200

    Best effort calls

--- src/ber_backend/src/lib.rs
+++ src/ber_backend/src/lib.rs
@@ -1,4 +1,25 @@
+use ic_cdk::prelude::*;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+
+#[ic_cdk::update]
+async fn tryme(i: u64) -> u64 {
+    if i == 0 && ic_cdk::api::call::msg_deadline() == 0 {
+	panic!("This should be a best-effort call!")
+    }
+    if i > 0 {
+    	let res: (u64,) = Call::new(ic_cdk::id(), "tryme")
+    	    .with_args((i - 1, ))
+    	    .change_timeout(1)
+    	    .call()
+    	    .await
+    	    .expect("Didn't expect the call to fail");
+    	res.0
+    } else {
+      42
+    }
 }
